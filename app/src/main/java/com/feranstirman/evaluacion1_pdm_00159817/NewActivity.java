df7688c9e9133a537d6aba133baeae117ac05512
NitroSkate <00159817@uca.edu.sv>
2019-03-29 17:58:13 -0600

    Ya envia datos a otras apps

--- app/src/main/java/com/feranstirman/evaluacion1_pdm_00159817/NewActivity.java
+++ app/src/main/java/com/feranstirman/evaluacion1_pdm_00159817/NewActivity.java
@@ -3,11 +3,13 @@ package com.feranstirman.evaluacion1_pdm_00159817;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NewActivity extends AppCompatActivity {
 
-    TextView i1,i2,i3,i4,i5,i6,i7,i8,i9,usuario,correo, total;
+    TextView i1, i2, i3, i4, i5, i6, i7, i8, i9, usuario, correo, total;
+    Button share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,11 @@ public class NewActivity extends AppCompatActivity {
         usuario = findViewById(R.id.usur);
         correo = findViewById(R.id.corr);
         total = findViewById(R.id.total);
+        share = findViewById(R.id.btn2);
 
         Intent mIntent = getIntent();
-        if(mIntent != null) {
-            String[] text = mIntent.getStringArrayExtra(Intent.EXTRA_TEXT);
+        String[] text = mIntent.getStringArrayExtra(Intent.EXTRA_TEXT);
+        if (mIntent != null) {
             i1.setText(text[0]);
             i2.setText(text[1]);
             i3.setText(text[2]);
@@ -43,5 +46,13 @@ public class NewActivity extends AppCompatActivity {
             total.setText(text[11]);
         }
 
+        share.setOnClickListener(e -> {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, text[0] + text[1] + text[2] + text[3] + text[4] + text[5] + text[6]
+                    + text[7] + text[8] + text[9] + text[10] + text[11]);
+            startActivity(intent);
+        });
     }
 }
